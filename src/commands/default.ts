06341675530c520001e6eef313ea63d0db938323
Breck Stodghill <b@ourzora.com>
2021-02-18 09:43:19 -0800

    [chore] reorganize subcommands

---
+++ src/commands/default.ts
@@ -0,0 +1,8 @@
+import {SubcommandDefinition} from 'clime';
+
+export const subcommands: SubcommandDefinition[] = [
+    {
+        name: 'mint',
+        brief: 'This group of subcommands will mint new zNFTs on the Zora Protocol. Type `zora mint --help` for more.',
+    },
+];

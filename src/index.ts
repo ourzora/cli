e08bb77239782429f7b1369ba2a2e1593436c85e
sporkspatula <72628669+sporkspatula@users.noreply.github.com>
2021-02-19 10:17:20 -0800

    [chore] remove clear (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import chalk from 'chalk'
-import clear from 'clear'
 import figlet from 'figlet'
 import { run } from './cli'
 
-clear()
 console.log(chalk.greenBright(figlet.textSync('zora-cli', { horizontalLayout: 'full' })))
 run()
